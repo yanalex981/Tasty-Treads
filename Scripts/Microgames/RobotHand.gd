55b3e49904643cf1c70c3b23e448e4af43a2f8bd
Kexin Huang <khuan24@uwo.ca>
2023-03-31 22:18:00 -0400

    Added hand movement and boundary box. Started making ingredients.

---
+++ Scripts/Microgames/RobotHand.gd
@@ -0,0 +1,29 @@
+extends CharacterBody2D
+
+@onready var _animation_player = $AnimationPlayer
+const SPEED := 400
+
+func _physics_process(delta):
+	# move the hand based on input
+	if Input.is_action_pressed("left"):
+		velocity.x = -SPEED
+	elif Input.is_action_pressed("right"):
+		velocity.x = SPEED
+	else:
+		velocity.x = 0
+	
+	if Input.is_action_pressed("down"):
+		velocity.y = SPEED
+	elif Input.is_action_pressed("up"):
+		velocity.y = -SPEED
+	else:
+		velocity.y = 0
+	
+	if Input.is_action_pressed("interact"):
+		grab_item()
+
+	velocity = velocity.normalized() * SPEED # normalize so that diagonals move at same speed
+	move_and_slide()
+	
+func grab_item():
+	_animation_player.play("grab_item")

ac027fdc72af9a9092afe431d61ee612e7ee804e
Kexin Huang <khuan24@uwo.ca>
2023-04-02 04:44:15 -0400

    Completed pouring minigame.
    User can adjust tilt within a set range. The angle of the tilt will make the pour faster or slower. Game ends on pan overflow or when the player presses space.

--- Scripts/Microgames/Pouring/RobotBowl.gd
+++ Scripts/Microgames/Pouring/RobotBowl.gd
@@ -1,11 +1,36 @@
 extends Node2D
 
+@onready var bowl = $Bowl
+@onready var dough = $Dough
 
-# Called when the node enters the scene tree for the first time.
+const MIN_TILT = -30
+const MAX_TILT = 45
+
+var cur_tilt = 0
+var tilt_rate = 1
+
+signal pour_started(tilt)
+signal pour_stopped
+
 func _ready():
-	pass # Replace with function body.
-
+	bowl.rotation_degrees = -45
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _process(_delta):
+	# tilt the hand based on input
+	if Input.is_action_pressed("left") && cur_tilt > MIN_TILT:
+		bowl.rotation_degrees -= tilt_rate
+	elif Input.is_action_pressed("right") && cur_tilt < MAX_TILT:
+		bowl.rotation_degrees += tilt_rate
+	
+	cur_tilt = bowl.rotation_degrees
+	
+	# show/hide the dough depending on the tilt angle
+	if cur_tilt > MIN_TILT:
+		dough.show()
+		emit_signal("pour_started", cur_tilt)
+	else:
+		dough.hide()
+		emit_signal("pour_stopped")
+
+

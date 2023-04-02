de4b18799c433fdf1de6b5f0769ceb07e4367c6a
Kexin Huang <khuan24@uwo.ca>
2023-04-02 00:34:59 -0400

    Cooling minigame now functional.
    Used other assets as placeholders for now.

---
+++ Scenes/Microgames/TimerLabel.gd
@@ -0,0 +1,13 @@
+extends Label
+
+@export var num_seconds : int 
+@onready var timer = $GameTimer
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	timer.wait_time = num_seconds
+	timer.start()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	text = "Time Left: " + str(int(timer.time_left))
--- Scripts/Microgames/Cooling/Smoke.gd
+++ Scripts/Microgames/Cooling/Smoke.gd
@@ -10,9 +10,9 @@ func _on_hurtbox_area_entered(area):
 
 func _on_hurtbox_area_exited(area):
 	if area.name == "Hitbox":
-		animation_player.pause("dissipating")
-
+		animation_player.pause()
 
 func _on_animation_player_animation_finished(anim_name):
 	if anim_name == "dissipating":
+		get_tree().root.get_child(0).get_child(0).decrease_active()
 		queue_free()

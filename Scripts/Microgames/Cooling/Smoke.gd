a9b6f30bf1a81a7d8f7f8bb9b2e2101966bb205a
Kexin Huang <khuan24@uwo.ca>
2023-04-02 18:02:50 -0400

    Adjusted cooling minigame to be more accurate to the prototype.
    Spawn more smoke and with random idle animations. Smoke hit once then disperses. Smoke hurtbox made smaller, fan hitbox made smaller. Fan now on by default and will always be on. Added vertical movement for fan.

--- Scripts/Microgames/Cooling/Smoke.gd
+++ Scripts/Microgames/Cooling/Smoke.gd
@@ -2,17 +2,21 @@ extends Node2D
 
 @onready var animation_player = $AnimationPlayer
 
+func _ready():
+	var idle_mode = randi_range(0,2)
+	if idle_mode == 0:
+		animation_player.play("idle")
+	elif idle_mode == 1:
+		animation_player.play("idle_2")
+	else:
+		animation_player.play("idle_3")
 
 func _on_hurtbox_area_entered(area):
 	if area.name == "Hitbox":
-		animation_player.play("dissipating")
+		animation_player.play("dissipate")
 
-
-func _on_hurtbox_area_exited(area):
-	if area.name == "Hitbox":
-		animation_player.pause()
 
 func _on_animation_player_animation_finished(anim_name):
-	if anim_name == "dissipating":
+	if anim_name == "dissipate":
 		get_tree().root.get_child(0).get_child(0).decrease_active()
 		queue_free()

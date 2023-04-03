9f3169439fb4a24069ef7914b40c1ba804843b14
Ian Ginson <iginson@uwo.ca>
2023-04-03 19:04:29 -0400

    Added stack of cookies
    When a cookie is successfully cut, it is added to a stack next to the pan

--- Scripts/Microgames/Cutting/BeatSpawner.gd
+++ Scripts/Microgames/Cutting/BeatSpawner.gd
@@ -2,8 +2,11 @@ extends Node2D
 
 @export var beat_scene: PackedScene
 @export var hole_scene: PackedScene
+@export var cookie_scene: PackedScene
+
 @onready var spawn_position = $"BeatSpawnLocation"
 @onready var hole_spawner = get_parent().get_parent().get_child(0).get_child(0) # Points to tree location DoughTray/CookieHoleSpawnPoints
+@onready var cookie_spawnpoint = get_parent().get_parent().get_child(2) # Points to tree location CutterMinigame/CookieSpawnPoint
 
 const NUM_BEATS: int = 12
 const TIME_TO_SPAWN: int = 4 # IMPORTANT: Needs to be a multiple of BEATS_TO_MOVE parameter from Beat class or timing messes up. Currently working with double value.
@@ -14,6 +17,7 @@ var totalBeatsSpawned = 0
 var activeBeats = []
 var spawnPointIndex = 0
 var holeSpawnLocations
+var cookieHeight = 10
 
 func _ready():
 	# Spawn the first beat
@@ -46,10 +50,21 @@ func calculate_points():
 	if activeBeats.size() > 0:
 		# Create a new hole at the appropriate location
 		var newHole = hole_scene.instantiate()
-		newHole.spriteType = activeBeats[0].beatType
+		var beatType = activeBeats[0].beatType 
+		newHole.spriteType = beatType
 		hole_spawner.add_child.call_deferred(newHole)
 		newHole.global_position = holeSpawnLocations[spawnPointIndex].position
 		spawnPointIndex += 1
+		
+		# Place a cookie if the beat was good or perfect
+		if beatType == "good" or beatType == "perfect":
+			var newCookie = cookie_scene.instantiate()
+			get_parent().get_parent().add_child.call_deferred(newCookie)
+			# Place the cookie at the spawn point
+			newCookie.global_position = cookie_spawnpoint.position
+			# Move the spawn point for the next cookie
+			cookie_spawnpoint.position.y -= cookieHeight
+		
 		# Remove the front beat from the queue
 		activeBeats.pop_front().delete()
 

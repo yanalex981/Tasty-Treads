160a557889bcee83fb83c0da3166219b91837389
Ian Ginson <iginson@uwo.ca>
2023-04-03 18:46:05 -0400

    Changed pan sprite
    Pan sprite updated with new artwork. Also spawns in cookie holes on successful timing of beats.

--- Scripts/Microgames/Cutting/BeatSpawner.gd
+++ Scripts/Microgames/Cutting/BeatSpawner.gd
@@ -1,7 +1,9 @@
 extends Node2D
 
 @export var beat_scene: PackedScene
+@export var hole_scene: PackedScene
 @onready var spawn_position = $"BeatSpawnLocation"
+@onready var hole_spawner = get_parent().get_parent().get_child(0).get_child(0) # Points to tree location DoughTray/CookieHoleSpawnPoints
 
 const NUM_BEATS: int = 12
 const TIME_TO_SPAWN: int = 4 # IMPORTANT: Needs to be a multiple of BEATS_TO_MOVE parameter from Beat class or timing messes up. Currently working with double value.
@@ -10,13 +12,18 @@ const TIME_INCREASE_VALUE: int = 10 #IMPORTANT: Needs to be a multiple of BEAT_S
 var timeSinceSpawn = 0
 var totalBeatsSpawned = 0
 var activeBeats = []
+var spawnPointIndex = 0
+var holeSpawnLocations
 
 func _ready():
+	# Spawn the first beat
 	var beat = beat_scene.instantiate()
 	get_parent().add_child.call_deferred(beat)
 	beat.global_position = spawn_position.position
 	totalBeatsSpawned += 1
 	activeBeats.push_back(beat)
+	# Set the hole spawn locations
+	holeSpawnLocations = hole_spawner.get_children()
 
 func _process(delta):
 	timeSinceSpawn += TIME_INCREASE_VALUE * delta
@@ -37,6 +44,13 @@ func _process(delta):
 
 func calculate_points():
 	if activeBeats.size() > 0:
+		# Create a new hole at the appropriate location
+		var newHole = hole_scene.instantiate()
+		newHole.spriteType = activeBeats[0].beatType
+		hole_spawner.add_child.call_deferred(newHole)
+		newHole.global_position = holeSpawnLocations[spawnPointIndex].position
+		spawnPointIndex += 1
+		# Remove the front beat from the queue
 		activeBeats.pop_front().delete()
 
 

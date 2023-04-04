2f56e0ff1e6c43dceef7809858392aa0a1af068a
alex <yanalex981@users.noreply.github.com>
2023-04-04 10:58:15 -0400

    fixed some frame stuff animation isn't tied to the keyboard anymore created separate player and droid classes (no path finding or recipe tracking yet)

--- Scripts/Components/AgentAnim.gd
+++ Scripts/Components/AgentAnim.gd
@@ -10,17 +10,3 @@ var direction : Vector2 = Vector2.ZERO:
 			set("parameters/idle/blend_position", value)
 		else:
 			get("parameters/playback").travel("idle")
-
-func _input(_event):
-	var new_direction = Vector2.ZERO
-	
-	if Input.is_action_pressed("up"):
-		new_direction += Vector2(0, -1)
-	if Input.is_action_pressed("down"):
-		new_direction += Vector2(0, 1)
-	if Input.is_action_pressed("left"):
-		new_direction += Vector2(-1, 0)
-	if Input.is_action_pressed("right"):
-		new_direction += Vector2(1, 0)
-
-	direction = new_direction.normalized()

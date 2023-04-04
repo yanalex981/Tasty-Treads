05fba1d43ecbacb2a7630a15f40ad6e76a3b247a
Kexin Huang <khuan24@uwo.ca>
2023-04-04 00:51:35 -0400

    Added the upgraded bar.
    Now changes texture and success range when upgraded.

--- Scripts/Microgames/Oven/OvenMicrogame.gd
+++ Scripts/Microgames/Oven/OvenMicrogame.gd
@@ -6,28 +6,32 @@ extends Node2D
 @onready var timer = $UI/GameTimer
 @onready var end_display = $UI/EndUI
 
+var up_bar_under = preload("res://Assets/Microgames/Minigame UI/ovenBarUnder2.png")
+var up_bar_progress = preload("res://Assets/Microgames/Minigame UI/ovenBarProgress2.png")
+var up_bar_over = preload("res://Assets/Microgames/Minigame UI/ovenBarFrame2.png")
+
 const UP_RATE : int = 5
-const UP_METRIC : Array[int] = []
+const UP_METRIC : Array[int] = [52, 81]
 const UP_TIME : int = 5
 
-var bake_metric : Array[int] = [55, 76] # 55 to 76 is perfect
+var bake_metric : Array[int] = [55, 76] # 55 to 75 is perfect
 var cook_time : int = 8 
 
 var success : bool = false
 signal game_ended(results)
 
 func _ready():
-	
 	if upgraded:
 		# increase speed
 		bake_bar.set_fill_rate(UP_RATE)
 		# change bar + metric
-		
-		#bake_metric = UP_METRIC
-		
+		bake_bar.texture_over = up_bar_over
+		bake_bar.texture_progress = up_bar_progress
+		bake_bar.texture_under = up_bar_under
+		bake_metric = UP_METRIC
 		# decrease cook time
 		cook_time = UP_TIME
-		
+	
 	timer.start(cook_time)
 
 func set_upgraded(status : bool):
